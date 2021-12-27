a8a34702aa2530625846223ec56a90e099d06564
Khushee-Upadhyay <khusheeupadhyay@gmail.com>
2021-12-27 18:28:53 +0530

    Add data processing script

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,19 +1,29 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
+import os
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    df = pd.read_csv(input_filepath)
+    # creating new feature by using age column
+
+    df["age_range"] = 1000
+    for i in range(len(df["age"])):
+        if df["age"][i] < 30:
+            df["age_range"][i] = 1
+        elif 30 <= df["age"][i] < 45:
+            df["age_range"][i] = 2
+        elif df["age"][i] >= 45:
+            df["age_range"][i] = 3
+
+    df["have_children"] = ["No" if i == 0 else "Yes" for i in df["children"]]
+    df.to_csv(output_filepath)
 
 
 if __name__ == '__main__':
@@ -21,10 +31,7 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    print(os.getcwd())
+    input_path = "data/raw/insurance.csv"
+    output_path = "data/processed/processed_insurance_data.csv"
+    main(input_path, output_path)
