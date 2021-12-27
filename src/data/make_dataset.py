799b5e9b7cfa70ef784b94669d11878a5de48d34
Khushee-Upadhyay <khusheeupadhyay@gmail.com>
2021-12-27 22:52:05 +0530

    Modifying data-preprocessing functions and adding data operations

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,29 +1,40 @@
 # -*- coding: utf-8 -*-
 import logging
-import pandas as pd
 import os
+from sklearn.preprocessing import LabelEncoder
+
+from src.util.csv_data_operations import load_insurance_data, save_insurance_data
 
 
-def main(input_filepath, output_filepath):
+def data_preprocessing():
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-    df = pd.read_csv(input_filepath)
+    # df = pd.DataFrame()
+    df = load_insurance_data()
     # creating new feature by using age column
 
     df["age_range"] = 1000
     for i in range(len(df["age"])):
         if df["age"][i] < 30:
             df["age_range"][i] = 1
-        elif 30 <= df["age"][i] < 45:
+        elif 30 <= df["age"][i] < 40:
             df["age_range"][i] = 2
-        elif df["age"][i] >= 45:
+        elif 40 <= df["age"][i] < 50:
             df["age_range"][i] = 3
+        elif df["age_range"][i] >= 50:
+            df["age_range"][i] = 4
 
     df["have_children"] = ["No" if i == 0 else "Yes" for i in df["children"]]
-    df.to_csv(output_filepath)
+
+    cat_variable = ['sex', 'smoker', 'region', 'have_children']
+    lb = LabelEncoder()
+    df[cat_variable] = df[cat_variable].apply(lambda col: lb.fit_transform(col.astype(str)))
+
+    save_insurance_data(df)
+    return
 
 
 if __name__ == '__main__':
@@ -32,6 +43,5 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     print(os.getcwd())
-    input_path = "data/raw/insurance.csv"
-    output_path = "data/processed/processed_insurance_data.csv"
-    main(input_path, output_path)
+
+    data_preprocessing()
---
+++ src/util/csv_data_operations.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def load_insurance_data():
+    input_path = "data/raw/insurance.csv"
+    df = pd.read_csv(input_path)
+    return df
+
+
+def save_insurance_data(df: pd.DataFrame):
+    output_path = "data/processed/processed_insurance_data.csv"
+    df.to_csv(output_path)
+    return
+
+
+def load_processed_insurance_data():
+    input_path = "data/processed/processed_insurance_data.csv"
+    df = pd.read_csv(input_path)
+    return df
+
+
+def save_train_test_data(Xtrain: pd.DataFrame, Ytrain: pd.DataFrame, Xtest: pd.DataFrame, Ytest: pd.DataFrame):
+    Xtrain.to_csv("data/model_training_data/training_data.csv")
+    Ytrain.to_csv("data/model_training_data/training_data_result.csv")
+    Xtest.to_csv("data/model_testing_data/testing_data.csv")
+    Ytest.to_csv("data/model_testing_data/testing_data_result.csv")
+    Ytest.to_csv("data/model_testing_data/testing_data_result.csv")
+    return
+
+
+def split_train_test_data():
+    df = load_processed_insurance_data()
+    X = df.drop(columns=["charges"])
+    # In the above line, the column sex is also dropped but let's see what's the effect of keeping sex on the
+    # invariance test
+    y = df["charges"]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    save_train_test_data(X_train, y_train, X_test, y_test)
