ac7008ddadc614f570bc67bf84941bb21589759f
Khushee-Upadhyay <khusheeupadhyay@gmail.com>
2021-12-30 18:23:14 +0530

    Update tests using pytest-check

--- tests/post_train_tests.py
+++ tests/post_train_tests.py
@@ -29,4 +29,5 @@ def test_sex_invariance(return_models):
         male_sample = [19, 0, 27.9, 0, 1, 2, 1, 1]
         result_female_sample = model.predict(np.array(female_sample).reshape(1, -1))
         result_male_sample = model.predict(np.array(male_sample).reshape(1, -1))
-        assert result_female_sample == result_male_sample
+        check.equal(result_female_sample, result_male_sample)
+        # assert result_female_sample == result_male_sample
